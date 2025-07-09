f1c56bd9d6df59ac199c0e0f3a895ac3995d9b4d
Jdyjjjiikhfgik55845 <Jdyjjjiikhfgik55845@gmail.com>
2025-07-09 22:01:47 +0530

    Update queue.py

--- PURVIMUSIC/utils/inline/queue.py
+++ PURVIMUSIC/utils/inline/queue.py
@@ -75,10 +75,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="˹❍ᴡηєʀ ᴀʙσᴜᴛ˼ ", url=f"https://t.me/THUNDERDEVS"
+                text="˹❍ᴡηєʀ ᴀʙσᴜᴛ˼ ", url=f"https://t.me/Jani_RP_Lover"
             ),
             InlineKeyboardButton(
-                text="˹ᴄʜᴧᴛ ɢʀσᴜᴘ˼", url=f"https://t.me/+PaEtaAu9DI9mYzc9"
+                text="˹ᴄʜᴧᴛ ɢʀσᴜᴘ˼", url=f"https://t.me/Jani_Chatting"
             ),
         ],
       ]
